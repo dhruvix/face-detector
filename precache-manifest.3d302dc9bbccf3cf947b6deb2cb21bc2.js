075769e5860cffa62dafa6c03ff4934681072892
dhruvix <dhruvix7@gmail.com>
2020-05-22 16:24:03 +0530

    Updates

---
+++ precache-manifest.3d302dc9bbccf3cf947b6deb2cb21bc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90cbbbb4d10d3b543d847c9509e13ef",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "86abece5db09dd1117a8",
+    "url": "/face-detector/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "dce38f8a1e446d2cb375",
+    "url": "/face-detector/static/css/main.747a56ba.chunk.css"
+  },
+  {
+    "revision": "86abece5db09dd1117a8",
+    "url": "/face-detector/static/js/2.ce4d735d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detector/static/js/2.ce4d735d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce38f8a1e446d2cb375",
+    "url": "/face-detector/static/js/main.f0086148.chunk.js"
+  },
+  {
+    "revision": "151ea045047a7135a99c",
+    "url": "/face-detector/static/js/runtime-main.c54fb57d.js"
+  },
+  {
+    "revision": "b351695092f922355b62dd3085435a01",
+    "url": "/face-detector/static/media/Rick.b3516950.png"
+  }
+]);
